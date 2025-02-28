427c1a1f46a94a850a951dbc8f93838e4b92eb6a
silvamaciel <macieljunio.si@gmail.com>
2025-02-28 02:05:00 -0300

    Consegui adicionar um switch case + do while para o cliente fazer algumas ações de deposito. Interessante

--- auladefixacao/src/application/Main.java
+++ auladefixacao/src/application/Main.java
@@ -1,9 +1,61 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.BanckAccounts;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Insert your account Number: ");
+		String accountNumber = sc.nextLine();
+
+		System.out.println("Insert your account Name: ");
+		String accountName = sc.nextLine();
+
+		System.out
+				.println("Insert The value to deposit in your account, if you don't want to do iet, please insert 0: ");
+		double accountBalance = sc.nextDouble();
+
+		BanckAccounts banckAccounts;
+
+		if (accountBalance > 0) {
+			banckAccounts = new BanckAccounts(accountNumber, accountName, accountBalance);
+		} else {
+			banckAccounts = new BanckAccounts(accountNumber, accountName);
+		}
+
+		System.out.println(banckAccounts.toString());
+
+		int choice;
+
+		do {
+			System.out.println(" 1 Para depositar | 2 para Sacar | 3 Para sair:");
+			choice = sc.nextInt();
+			
+			switch(choice) {
+			case 1:
+				System.out.println("Insira o valor para depósito");
+				banckAccounts.DepositAccount(sc.nextDouble());
+				System.out.println("Update Account Balance");
+				System.out.println(banckAccounts.toString());
+				break;
+			case 2:
+				System.out.println("Insert the value to your bank withdrawal");
+				banckAccounts.withdrawalAccount(sc.nextDouble());
+				System.out.println("Update Account Balance");
+				System.out.println(banckAccounts.toString());
+				break;
+			case 3: 
+				System.out.println("Obrigado e volte Sempre !");
+				break;
+			default: System.out.println("Opção Invalida!");
+			}
+
+		} while (choice != 3);
+
 	}
 
 }
--- auladefixacao/src/entities/BanckAccounts.java
+++ auladefixacao/src/entities/BanckAccounts.java
@@ -1,22 +1,22 @@
 package entities;
 
 public class BanckAccounts {
-	
+
 	private String accountNumber;
 	private String accountName;
 	private double accountBalance;
-	
-	public BanckAccounts () {
-		
+	private double txBank = 5;
+	public BanckAccounts() {
+
 	}
-	
+
 	public BanckAccounts(String accountNumber, String accountName, double accountBalance) {
 		super();
 		this.accountNumber = accountNumber;
 		this.accountName = accountName;
 		this.accountBalance = accountBalance;
 	}
-	
+
 	public BanckAccounts(String accountNumber, String accountName) {
 		super();
 		this.accountNumber = accountNumber;
@@ -38,21 +38,20 @@ public class BanckAccounts {
 	public double getAccountBalance() {
 		return accountBalance;
 	}
-	
-	public double DepositAccount (double accountBalance) {
+
+	public double DepositAccount(double accountBalance) {
 		return this.accountBalance += accountBalance;
 	}
-	
-	public double withdrawalAccount (double accountBalance) {
-		return this.accountBalance -= accountBalance;
+
+	public double withdrawalAccount(double accountBalance) {
+		return this.accountBalance -= accountBalance-txBank;
 	}
 
 	@Override
 	public String toString() {
-		return "BanckAccounts [getAccountNumber()=" + getAccountNumber() + ", getAccountName()=" + getAccountName()
-				+ ", getAccountBalance()=" + getAccountBalance() + "]";
+		return "Your Account Number: " + getAccountNumber() 
+				+ ",\n Account Name: " + getAccountName() 
+				+ ",\n Account Balance = R$"	+ getAccountBalance();
 	}
-	
-	
-	
+
 }
